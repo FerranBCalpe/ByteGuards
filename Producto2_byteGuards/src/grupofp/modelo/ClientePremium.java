ce9569881687105f45e5cf64d44d94aa1b37ed24
Carlos Ruz <carlosruzdomingo@gmail.com>
2025-03-26 02:37:51 +0100

    Delete Producto2_byteGuards directory

--- Producto2_byteGuards/src/grupofp/modelo/ClientePremium.java
+++
@@ -1,9 +0,0 @@
-package grupofp.modelo;
-
-public class ClientePremium extends Cliente {
-    public ClientePremium (String nombre, String domicilio, String nif, String email){
-        super(nombre, domicilio, nif, email);
-        this.cuotaAnual=30.0;
-        this.descuentoEnvio=0.2;
-    }
-}
